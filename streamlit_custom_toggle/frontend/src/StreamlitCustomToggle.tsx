ed54f933ebee12b428d05c84bde98e44a1668ab3
Shruti Agarwal <82811717+ShruAgarwal@users.noreply.github.com>
2023-02-22 16:23:41 +0530

    Made changes as per v0.1.1

--- streamlit_custom_toggle/frontend/src/StreamlitCustomToggle.tsx
+++ streamlit_custom_toggle/frontend/src/StreamlitCustomToggle.tsx
@@ -5,38 +5,46 @@ import { HeartSwitch } from "@anatoliygatt/heart-switch";
 
 interface PythonArgs {
   label: string
+  default_value: boolean
   value: boolean
   active_track_fill: string
   active_thumb_color: string
 }
   
 const StreamlitCustomToggle = (props: ComponentProps) => {
-  const { label, value, active_track_fill, active_thumb_color}: PythonArgs = props.args;
-  const [checked, setChecked] = useState(false);
+  const { label, default_value, value, active_track_fill, active_thumb_color}: PythonArgs = props.args;
   useEffect(() => Streamlit.setFrameHeight());
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+    Streamlit.setComponentValue(!event.target.checked );
+  };
+  const [state, setState] = React.useState({
+      checkStatus: default_value
+  });
+  
   
     return (
       <>
       {label}
-
+      
       
         <HeartSwitch
         size="md"
+        disabled={value}
         inactiveTrackFillColor="#cffafe"
         inactiveTrackStrokeColor="#080015"
         activeTrackFillColor={active_track_fill}
         activeTrackStrokeColor="#0891b2"
         inactiveThumbColor="#ecfeff"
         activeThumbColor={active_thumb_color}
-        checked={checked}
-        onChange={(event) => {
-          setChecked(event.target.checked);
-        }}
-        aria-disabled={value}
+        checked={state.checkStatus}
+        onChange={handleChange}
+        name="checkStatus"
       /> 
       </> 
     );
-};
+}
 
 // The function publicly available.
 export default withStreamlitConnection(StreamlitCustomToggle);
