6d9decd012ef5e8afca0546b418ec7b043d3471d
Shruti Agarwal <82811717+ShruAgarwal@users.noreply.github.com>
2023-02-22 16:40:49 +0530

    changes made as per v0.1.1

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
+from os.path import dirname
+from os.path import join
 import setuptools
+
+def readme() -> str:
+    
+    """Utility function to read the README file.
+    Used for the long_description.
+    :return: content of README.md
+    """
+    
+    return open(join(dirname(__file__), "README.md")).read()
 
 setuptools.setup(
     name="streamlit-custom-toggle",
-    version="0.0.1",
+    version="0.1.1",
     author="Shruti Agarwal",
     author_email="shruagarwal01@gmail.com",
     description="A Streamlit custom component to load heart-shaped Toggle Switch",
-    long_description="",
-    long_description_content_type="text/plain",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
     url="https://github.com/ShruAgarwal/streamlit-custom-toggle",
     packages=setuptools.find_packages(),
     include_package_data=True,
